a355bbfc25aa820d0f6ddc21e825f77f8476445a
aaryaman <78210804+aaryaman22@users.noreply.github.com>
2023-06-05 22:33:56 +0530

    Netflix Clone
    Added Folder

---
+++ NetFit-main/src/components/index.js
@@ -0,0 +1,7 @@
+export {default as Cart} from './pages/Cart'
+export {default as Diet} from './pages/Diet'
+export {default as Home} from './pages/Home'
+export {default as Shop} from './pages/Shop'
+export {default as Workout} from './pages/Workout'
+
+
